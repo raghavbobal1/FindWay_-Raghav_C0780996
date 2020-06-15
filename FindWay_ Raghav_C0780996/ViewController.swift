7684906a397e35d727c7863e99e5e6f73ffd1e5f
raghavbobal1 <raghavbobal001@gmail.com>
2020-06-14 21:39:31 -0400

    [Raghav] Added buttons, images etc

--- FindWay_ Raghav_C0780996/ViewController.swift
+++ FindWay_ Raghav_C0780996/ViewController.swift
@@ -9,120 +9,147 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+class ViewController: UIViewController , CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    var destination: CLLocationCoordinate2D!
+    var taplocation: CLLocationCoordinate2D?
     
     @IBOutlet weak var map: MKMapView!
-    var locationManager = CLLocationManager()
-
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         map.delegate=self
-        
-        //giving the delegate of locationManager to this class
-        locationManager.delegate = self
-        
+    
+    
         // accuracy of the location
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         
-        //req the user to grant location access
-        locationManager.requestWhenInUseAuthorization()
-        
-        //start updating the location of the user
+        // start updating the location of the user
         locationManager.startUpdatingLocation()
         
-        //1- define lat and long
+        // request the user for the location access
+        locationManager.requestWhenInUseAuthorization()
+    
+        // giving the delegate of locationManager to this class
+        locationManager.delegate = self
+     
+        
+        // step 1 - declaring the latitude and longitude
         let latitude: CLLocationDegrees = 43.64
         let longitude: CLLocationDegrees = -79.38
+  
+    displayLocation(latitude: latitude, longitude: longitude, title: "Downtown,Toronto", subtitle: "Happening atmosphere")
         
-        
-          displayLocation(latitude: latitude, longitude: longitude, title: "Downtown,Toronto", subtitle: "beatiful city")
-        
-        
-        //for long press gesture
+        // long press gesture
         let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(addlongPressAnnotation))
         map.addGestureRecognizer(uilpgr)
         
+        // add double tap
         addDoubleTap()
         
-        //MARK: - adds long press gesture recogniser for the annotation
-        @objc func addlongPressAnnotation(gestureRecognizer: UIGestureRecognizer)
-        {
-            let touchPoint = gestureRecognizer.location(in: map)
-            let coordinate = map.convert(touchPoint, toCoordinateFrom: map)
-            
-            //add annotation
-            let annotation = MKPointAnnotation()
-            annotation.title = "My destination"
-            annotation.coordinate = coordinate
-            map.addAnnotation(annotation)
+    }
+    
+    //MARK: - adding long press gesture recognizer for the annotation
+    @objc func addlongPressAnnotation(gestureRecognizer: UIGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(in: map)
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: map)
+        
+        // add annotation
+        let annotation = MKPointAnnotation()
+        annotation.title = "My destination"
+        annotation.coordinate = coordinate
+        map.addAnnotation(annotation)
+    }
+    
+    
+    //MARK: - didupdatelocation method
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        displayLocation(latitude: latitude, longitude: longitude, title: "Your Location", subtitle: "you are here")
+        
+        
+    }
+    
+    //MARK: - display user location method
+    func displayLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, title: String, subtitle: String) {
+        // step 2 - define delta latitude and delta longitude for the span
+        let latDelta: CLLocationDegrees = 0.5
+        let lngDelta: CLLocationDegrees = 0.5
+        
+        // step 3 - creating the span and location coordinate and finally the region
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta)
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        // step 4 - set region for the map
+        map.setRegion(region, animated: true)
+        
+        // add annotation
+        let annotation = MKPointAnnotation()
+        annotation.title = title
+        annotation.subtitle = subtitle
+        annotation.coordinate = location
+        map.addAnnotation(annotation)
+    }
+    
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin))
+        doubleTap.numberOfTapsRequired = 2
+        map.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func dropPin(sender: UITapGestureRecognizer)
+    {
+        removePin()
+        
+        // add annotation
+        
+        let touchPoint = sender.location(in: map)
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: map)
+        let annotation = MKPointAnnotation()
+        annotation.title = "My destination"
+        annotation.coordinate = coordinate
+        map.addAnnotation(annotation)
+    }
+    
+    func removePin() {
+        for annotation in map.annotations {
+            map.removeAnnotation(annotation)
+        }
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    //MARK: - add viewFor annotation method
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            return nil
         }
         
-        //MARK: - didupdatelocation method
-        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            let userLocation = locations[0]
-            
-            let latitude = userLocation.coordinate.latitude
-            let longitude = userLocation.coordinate.longitude
-            
-            displayLocation(latitude: latitude, longitude: longitude, title: "Your Location", subtitle: "This is where you are as of now")
-\
-            //MARK: - display user location method
-               func displayLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, title: String, subtitle: String) {
-                   // defining the delta latitude and delta longitude for the span
-                   let latDelta: CLLocationDegrees = 0.05
-                   let lngDelta: CLLocationDegrees = 0.05
-                   
-                   //creating the span and location coordinate and finally the region
-                   let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta)
-                   let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                   let region = MKCoordinateRegion(center: location, span: span)
-                   
-                   // set region for the map
-                   map.setRegion(region, animated: true)
-                   
-                   // add annotation
-                   let annotation = MKPointAnnotation()
-                   annotation.title = title
-                   annotation.subtitle = subtitle
-                   annotation.coordinate = location
-                   map.addAnnotation(annotation)
-               }
-        
-            
-            func addDoubleTap() {
-                let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin))
-                doubleTap.numberOfTapsRequired = 2
-                map.addGestureRecognizer(doubleTap)
-            }
-            
-            @objc func dropPin(sender: UITapGestureRecognizer) {
-                removePin()
-                
-                // add annotation
-                
-                let touchPoint = sender.location(in: map)
-                let coordinate = map.convert(touchPoint, toCoordinateFrom: map)
-                let annotation = MKPointAnnotation()
-                annotation.title = "My destination"
-                annotation.coordinate = coordinate
-                map.addAnnotation(annotation)
-            }
-             
-                func removePin()
-                {
-                    for annotation in map.annotations
-                    {
-                        map.removeAnnotation(annotation)
-                    }
-                }
-            }
-
-            
-            
+        // add custom annotation with image
+        let pinAnnotation = map.dequeueReusableAnnotationView(withIdentifier: "droppablePin") ?? MKPinAnnotationView()
+        pinAnnotation.image = UIImage(named: "ic_place_2x")
+        pinAnnotation.canShowCallout = true
+        pinAnnotation.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        return pinAnnotation
     }
-        
     
-
+    //MARK: - callout accessory control tapped
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let alertController = UIAlertController(title: "Your Place", message: "Welcome", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
+
 
