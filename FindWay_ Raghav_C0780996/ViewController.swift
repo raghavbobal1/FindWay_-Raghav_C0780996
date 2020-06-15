cc6b5545a16daf153bee7c3599e50699d6a30d4f
raghavbobal1 <raghavbobal001@gmail.com>
2020-06-15 00:53:08 -0400

    [Raghav] added new function to add annotation

--- FindWay_ Raghav_C0780996/ViewController.swift
+++ FindWay_ Raghav_C0780996/ViewController.swift
@@ -62,7 +62,7 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
                //to get the users location
                location = locations.first!
                let coordinateRegion = MKCoordinateRegion(center: location!.coordinate, latitudinalMeters: 1000, longitudinalMeters:1000)
-               mapView.setRegion(coordinateRegion, animated: true)
+               map.setRegion(coordinateRegion, animated: true)
                locationManager.stopUpdatingLocation()
            }
     
@@ -76,100 +76,31 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
     
     @objc func doubleTap(sender: UITapGestureRecognizer)
          {
-            // Getting coordinate of double tapped point and adding annotation
+            // Getting coordinates of the tapped point to add annotation
              let locationInView = sender.location(in: map)
              let locationOnMap = map.convert(locationInView, toCoordinateFrom: map)
              addAnnotation(location: locationOnMap)
              getLocationInfo()
          }
     
+    func addAnnotation(location: CLLocationCoordinate2D)
+           {
+               //Removing previous annotations and route
+               self.map.removeOverlays(self.map.overlays)
+               let oldAnnotations = self.map.annotations
+               self.map.removeAnnotations(oldAnnotations)
+               
+               //Adding new annotation
+               let annotation = MKPointAnnotation()
+               annotation.coordinate = location
+               latitude = annotation.coordinate.latitude
+               longitude = annotation.coordinate.longitude
+               annotation.title = "Your Dstination"
+               
+               self.map.addAnnotation(annotation)
+           }
     
     
-    //MARK: - didupdatelocation method
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation = locations[0]
-        
-        let latitude = userLocation.coordinate.latitude
-        let longitude = userLocation.coordinate.longitude
-        
-        displayLocation(latitude: latitude, longitude: longitude, title: "Your Location", subtitle: "you are here")
-        
-        
-    }
-    
-    //MARK: - display user location method
-    func displayLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, title: String, subtitle: String) {
-        // step 2 - define delta latitude and delta longitude for the span
-        let latDelta: CLLocationDegrees = 0.5
-        let lngDelta: CLLocationDegrees = 0.5
-        
-        // step 3 - creating the span and location coordinate and finally the region
-        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta)
-        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let region = MKCoordinateRegion(center: location, span: span)
-        
-        // step 4 - set region for the map
-        map.setRegion(region, animated: true)
-        
-        // add annotation
-        let annotation = MKPointAnnotation()
-        annotation.title = title
-        annotation.subtitle = subtitle
-        annotation.coordinate = location
-        map.addAnnotation(annotation)
-    }
-    
-    
-    func addDoubleTap() {
-        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin))
-        doubleTap.numberOfTapsRequired = 2
-        map.addGestureRecognizer(doubleTap)
-    }
-    
-    @objc func dropPin(sender: UITapGestureRecognizer)
-    {
-        removePin()
-        
-        // add annotation
-        
-        let touchPoint = sender.location(in: map)
-        let coordinate = map.convert(touchPoint, toCoordinateFrom: map)
-        let annotation = MKPointAnnotation()
-        annotation.title = "My destination"
-        annotation.coordinate = coordinate
-        map.addAnnotation(annotation)
-    }
-    
-    func removePin() {
-        for annotation in map.annotations {
-            map.removeAnnotation(annotation)
-        }
-    }
-}
 
-extension ViewController: MKMapViewDelegate {
-    //MARK: - add viewFor annotation method
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        if annotation is MKUserLocation {
-            return nil
-        }
-        
-        // add custom annotation with image
-        let pinAnnotation = map.dequeueReusableAnnotationView(withIdentifier: "droppablePin") ?? MKPinAnnotationView()
-        pinAnnotation.image = UIImage(named: "ic_place_2x")
-        pinAnnotation.canShowCallout = true
-        pinAnnotation.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        return pinAnnotation
-    }
     
-    //MARK: - callout accessory control tapped
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        let alertController = UIAlertController(title: "Your Place", message: "Welcome", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        alertController.addAction(cancelAction)
-        present(alertController, animated: true, completion: nil)
-    }
-}
-
-
+  
